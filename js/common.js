d369de9d9dae8033974d12d742a901f1a8c7ed7f
fengshangwuqi <billydong@foxmail.com>
2016-10-18 08:51:41 +0800

    注册登陆

--- js/common.js
+++ js/common.js
@@ -11,3 +11,31 @@ function reurl(){
 var getClientHeight = function (){
   return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 };
+
+var getClientWidth = function (){
+  return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+};
+
+var smallScreen = function(){
+  return getClientWidth() >= 768 ? true : false;
+}
+
+var formCheck = function(selector, flag){
+  if(flag == false){
+    selector.on("submit", function(event){
+      event.preventDefault();
+      $(".form-error").show();
+    });
+  }
+  if(flag == true){
+    selector.off("submit");
+  }
+}
+
+jQuery.fn.addAnimate=function(animation){
+  $(this).addClass('animated ' + animation);
+}
+
+jQuery.fn.removeAnimate=function(animation){
+  $(this).removeClass('animated ' + animation);
+}
--- js/sign.js
+++ js/sign.js
@@ -3,18 +3,7 @@ jQuery.fn.formChange=function(){
   $(this).siblings("form").hide();
   $(this).show();
   $(this).find(".form-focus").focus();
-}
-
-var formCheck = function(selector, flag){
-  if(flag == false){
-    selector.on("submit", function(event){
-      event.preventDefault();
-      $(".form-error").show();
-    });
-  }
-  if(flag == true){
-    selector.off("submit");
-  }
+  $(".form-error").hide();
 }
 
 $(".btn_extra").click(function(){
@@ -31,10 +20,10 @@ $('.form-autofill').on('input',function(){
   $(this).removeClass("form-autofill");
 });
 
-$("#logIn .btn").click(function(e){
+$(".btn").click(function(e){
   if($(this).prevAll(".pwd").children("input").val().length < 6){
     $(".form-error").text("密码至少6位");
-    formCheck($("#logIn"), false);
+    formCheck($(this).parent(), false);
   }else{
     formCheck($("#logIn"), true);
   }
